eca57ae23b3330bc745cd5aa3e7b2120535a5136
Fabin-27 <fabiolourenco063@gmail.com>
2024-03-28 09:07:04 -0300

    Projeto orientado a objetos finalizado

--- src/entitites/Calculo_decimal.java
+++ src/entitites/Calculo_decimal.java
@@ -1,34 +1,9 @@
 package entitites;
 
 public class Calculo_decimal {
-	
-	private int decimal = 0;
-	private int potencia = 0;
-	
+
 	public Calculo_decimal() {
 
-	}
-
-	public Calculo_decimal(int decimal, int potencia) {
-		super();
-		this.decimal = decimal;
-		this.potencia = potencia;
-	}
-
-	public int getDecimal() {
-		return decimal;
-	}
-
-	public void setDecimal(int decimal) {
-		this.decimal = decimal;
-	}
-
-	public int getPotencia() {
-		return potencia;
-	}
-
-	public void setPotencia(int potencia) {
-		this.potencia = potencia;
 	}
 
 	public int binaryToDecimal(String binaryString) {
